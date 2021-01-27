3602a3f2f137f4973e0b4815913a1d011a17a0b5
LastHouse <54175608+LastHouse@users.noreply.github.com>
2021-01-27 16:50:56 +0200

    Part 9.4 done

--- fso2020_ts/index.ts
+++ fso2020_ts/index.ts
@@ -3,7 +3,7 @@ import express = require('express');
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('Hello Full Stack!');
+  res.send('Hello Full Stack!!!');
 });
 
 const PORT = 3005;
